6eb3465f8515f0b206af49b471ee69b0747be259
Herman Andres <heanfig@gmail.com>
2022-02-10 09:35:17 -0500

    initial commit

---
+++ apps/migracion-sim-front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'migracion-sim-front-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'migracion-sim-front';
+}
