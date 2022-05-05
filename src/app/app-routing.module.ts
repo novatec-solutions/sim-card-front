8eafe43cc5c0f3581cba2e82b820cb876691bc29
Herman Andres <heanfig@gmail.com>
2022-05-04 21:00:56 -0500

    refactor(general): depuracion de rutas del aplicativo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule)
-  },
-  {
-    path: 'pin',
-    loadChildren: () =>
       import('./modules/pin/pin.module').then((m) => m.PinModule),
-  },
-  {
-    path: 'cuenta',
-    loadChildren: () =>
-      import('./modules/account/account.module').then((m) => m.AccountModule),
-  },
-  {
-    path: 'soporte',
-    loadChildren: () =>
-      import('./modules/failure-support/failure-support.module').then((m) => m.FailureSupportModule),
   },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
