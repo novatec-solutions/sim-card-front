938f36429ee50a683d9a5b0d6c7c3734bcdbd476
Marcela Vaquiro <marcela.vaquiro@novatec.com.co>
2022-05-09 12:24:18 -0500

    (fix): Se ajuta ruta dist

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sim-masivas-front'),
+      dir: require('path').join(__dirname, './coverage/sim-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
