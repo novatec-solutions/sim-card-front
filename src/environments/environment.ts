8a0dcb78d81682c5b1da16fc0c7581f15b9fc8c8
Maria Alejandra Sánchez Román <maria.sanchez@novatec.loc>
2022-06-13 13:17:39 -0500

    fix: consumo y data de prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   url: "http://simcard-back-miclaro-dev-novatec.104.209.147.150.nip.io/",
-  mockUrl: 'https://cca1bc54-7ece-48a7-932b-cd1839a39284.mock.pstmn.io/',
   iv: 'abcdef9876543210abcdef9876543210',
   secretKey: '0123456789abcdef0123456789abcdef'
 };
